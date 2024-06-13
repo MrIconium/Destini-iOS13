4772cb53ffb8752e5cb4f38f514cc70b7234695f
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-06-13 11:21:49 +0300

    Add logic and finish the project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StoryBrainDelegate {
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI(story.stories[0])
+        story.delegate = self
     }
 
-
+    @IBAction func choiceSelected(_ sender: UIButton) {
+        story.checkChoice(sender.titleLabel!.text!, story.currentDestination)
+    }
+    
+    func updateUI(_ storyStep:Story){
+        storyLabel.text = storyStep.title
+        choice1Button.setTitle(storyStep.choice1, for: .normal)
+        choice2Button.setTitle(storyStep.choice2, for: .normal)
+    }
 }
 
